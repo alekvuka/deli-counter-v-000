630c48512e2f5e9b66d05eeaa98f03ed2d066dd3
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 23:37:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def line(katz_deli)
+
+  phrase_array = Array.new
+  counter = 1
+
+  katz_deli.each do |customer|
+    phrase_array = "#{counter}. #{customer}"
+    counter += 1
+  end
+
+  phrase = phrase_array.join
+
+end
