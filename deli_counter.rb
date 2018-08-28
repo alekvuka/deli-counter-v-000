ee6466dc24057cf66429c4e32bf1721bb84238ab
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-28 23:56:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -39,8 +39,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number 1 in line."
     katz_deli.push(name)
   else
-    puts "Welcome, #{name}. You are number #{katz_deli.size} in line. 
-
-
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  end 
 
 end
